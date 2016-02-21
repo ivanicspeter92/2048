302406e0ae7698e34b33abefe92927902948fbae
Peter Ivancis <peter.ivanics@reslink.fi>
2016-02-21 16:19:44 +0200

    Made tests pass.

--- 2048Java/src/TwentyFourtyEight.java
+++ 2048Java/src/TwentyFourtyEight.java
@@ -1,50 +1,167 @@
+
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author PéterIvanics
  */
-public class TwentyFourtyEight implements ITwentyFourtyEight
+public final class TwentyFourtyEight implements ITwentyFourtyEight
 {
+    private int boardWidth = 4;
+    private int boardHeight = 4;
     /// The 4*4 board containing the current state of the game.
-    private int[][] board = new int[4][4];
+    private int[][] board = new int[boardWidth][boardHeight];
+   
+    /**
+     * Initializes the game with specified board width and height. Both values must be higher than 1.
+     * 
+     * @param width The width of the board.
+     * @param height The height of the board.
+     * 
+     * @author Péter Ivanics
+     * @date 01.02.2016.
+     */
+    public TwentyFourtyEight(int width, int height)
+    {
+        if (width > 1 && height > 1)
+        {
+            this.boardWidth = width;
+            this.boardHeight = height;
+        }
+        
+        this.restart();
+    }
+    
+    /**
+     * Initializes the game with an existing board. Does not restart the game, loads the present values in the matrix to the game's state.
+     * 
+     * @param board The board to be loaded to the 2048 game.
+     * 
+     * @author Péter Ivanics
+     * @date 01.02.2016.
+     */
+    public TwentyFourtyEight(int[][] board)
+    {
+        this.board = this.deepCopy(board);
+        this.boardWidth = board.length;
+        this.boardHeight = board[0].length;
+    }
+    
+    /**
+     * Initializes a 4*4 board.
+     * 
+     * @author Péter Ivanics
+     * @date 01.02.2016.
+     */
+    public TwentyFourtyEight()
+    {
+        this.restart();
+    }
     
     /**
      * Swipes the board from bottom to top.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     public void swipeUp()
     {
+        for (int i = this.boardHeight - 1; i > 0; i--) 
+        {
+            for (int j = this.boardWidth - 1; j > -1; j--) 
+            {
+                if (this.isEmpty(i - 1, j))
+                {
+                    this.board[i - 1][j] = this.board[i][j];
+                    this.board[i][j] = 0;
+                }
+                else if (this.board[i - 1][j] == this.board[i][j])
+                {
+                    this.board[i - 1][j] = this.board[i - 1][j] + this.board[i][j];
+                    this.board[i][j] = 0;
+                }
+            }
+        }
     }  
     
    /**
      * Swipes the board from top to bottom.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     public void swipeDown()
     {
+        for (int i = 0; i < this.boardHeight - 1; i++) 
+        {
+            for (int j = 0; j < this.boardWidth; j++) 
+            {
+                if (this.isEmpty(i + 1, j))
+                {
+                    this.board[i + 1][j] = this.board[i][j];
+                    this.board[i][j] = 0;
+                }
+                else if (this.board[i + 1][j] == this.board[i][j])
+                {
+                    this.board[i + 1][j] = this.board[i + 1][j] + this.board[i][j];
+                    this.board[i][j] = 0;
+                }
+            }
+        }
     }  
     
     /**
      * Swipes the board from right to left.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 02.01.2016.
      */
     public void swipeLeft()
     {
+        for (int i = this.boardHeight - 1; i > -1; i--) 
+        {
+            for (int j = this.boardWidth - 1; j > 0; j--) 
+            {
+                if (this.isEmpty(i, j - 1))
+                {
+                    this.board[i][j - 1] = this.board[i][j];
+                    this.board[i][j] = 0;
+                }
+                else if (this.board[i][j - 1] == this.board[i][j])
+                {
+                    this.board[i][j - 1] = this.board[i][j - 1] + this.board[i][j];
+                    this.board[i][j] = 0;
+                    j += 2;
+                }
+            }
+        }
     }  
     
     /**
      * Swipes the board from left to right.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     public void swipeRight()
     {
+        for (int i = 0; i < this.boardHeight; i++) 
+        {
+            for (int j = 0; j < this.boardWidth - 1; j++) 
+            {
+                if (this.isEmpty(i, j + 1))
+                {
+                    this.board[i][j + 1] = this.board[i][j];
+                    this.board[i][j] = 0;
+                }
+                else if (this.board[i][j + 1] == this.board[i][j])
+                {
+                    this.board[i][j + 1] = this.board[i][j + 1] + this.board[i][j];
+                    this.board[i][j] = 0;
+                }
+            }
+        }
     }            
     
     /**
@@ -53,10 +170,33 @@ public class TwentyFourtyEight implements ITwentyFourtyEight
      * @return True, if there are no possible moves left to be performed on the board; false otherwise.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     public boolean isOver()
     {
+        if (this.countEmptyCells() == 0)
+        {            
+            TwentyFourtyEight nextStepUp = new TwentyFourtyEight(board);
+            nextStepUp.swipeUp();
+            TwentyFourtyEight nextStepDown = new TwentyFourtyEight(board);
+            nextStepDown.swipeDown();
+            TwentyFourtyEight nextStepLeft = new TwentyFourtyEight(board);
+            nextStepLeft.swipeLeft();
+            TwentyFourtyEight nextStepRight = new TwentyFourtyEight(board);
+            nextStepRight.swipeRight();
+            
+            int[][] upBoard = nextStepUp.getBoard(), downBoard = nextStepDown.getBoard(), leftBoard = nextStepLeft.getBoard(), rightBoard = nextStepRight.getBoard();
+            
+            boolean result = true;
+            
+            result = result && Arrays.deepEquals(upBoard, this.board);
+            result = result && Arrays.deepEquals(downBoard, this.board);
+            result = result && Arrays.deepEquals(leftBoard, this.board);
+            result = result && Arrays.deepEquals(rightBoard, this.board);
+            
+            return result;
+        }
+        
         return false;
     }
     
@@ -64,11 +204,24 @@ public class TwentyFourtyEight implements ITwentyFourtyEight
      * Restarts the game, resets the board to its original state.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     public void restart()
     {
-    
+        this.board = new int[this.boardWidth][this.boardHeight];
+        
+        int x = 0, y = 0, count = 0;
+        while (count < 2)
+        {
+            x = this.getRandomX(); 
+            y = this.getRandomY();
+            
+            if (this.isEmpty(x, y))
+            {
+                this.board[x][y] = 2;
+                count++;
+            }
+        }
     }
     
     /**
@@ -77,10 +230,96 @@ public class TwentyFourtyEight implements ITwentyFourtyEight
      * @return The matrix of the board in its current state.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
+    @Override
     public int[][] getBoard()
     {
         return this.board;
     }
+    
+    /******* Private methods *******/    
+    /**
+     * Gets a random coordinate in the horizontal axis of the board.
+     * @return A random number between zero and the board's width. 
+     * 
+     * @author Péter Ivanics
+     * @date 01.02.2016.
+     */
+    private int getRandomX()
+    {
+        Random rnd = new Random();
+        
+        return rnd.nextInt(this.boardWidth);
+    }
+    
+   /**
+     * Gets a random coordinate in the vertical axis of the board.
+     * @return A random coordinate between zero and the board's height. 
+     * 
+     * @author Péter Ivanics
+     * @date 01.02.2016.
+     */
+    private int getRandomY()
+    {
+        Random rnd = new Random();
+        
+        return rnd.nextInt(this.boardHeight);
+    }
+    
+    /**
+     * Tells if the tile on the board at the specified coordinates is empty or not.
+     * @param x The x coordinate of the tile.
+     * @param y The y coordinate of the tile.
+     * @return True if the tile on coordinates (x,y) on the board is empty; false otherwise.
+     * 
+     * @author Péter Ivanics
+     * @date 01.02.2016.
+     */
+    private boolean isEmpty(int x, int y)
+    {
+        if (this.board[x][y] == 0)
+            return true;
+        
+        return false;
+    }
+    
+    /**
+     * Counts the empty cells on the board.
+     * 
+     * @return The number of empty cells on the board.
+     * 
+     * @author Péter Ivanics
+     * @date 02.01.2016.
+     */
+    private int countEmptyCells()
+    {
+        int count = 0;
+        
+        for (int i = 0; i < this.boardHeight; i++) 
+        {
+            for (int j = 0; j < this.boardWidth - 1; j++) 
+            {
+                if (this.isEmpty(i, j))
+                    count++;
+            }
+        }
+         return count;
+    }
+    
+    /**
+     * Creates a deep copy of the given array and returns it to the caller.
+     * @param input The two dimensional array of integers to be copied. 
+     * @return A deep copy of the given array.
+     * 
+     * @author Peter Ivanics
+     * @date 21.02.2016.
+     */
+    private int[][] deepCopy(int[][] input) {
+      int[][] target = new int[input.length][];
+      for (int i=0; i <input.length; i++) {
+        target[i] = Arrays.copyOf(input[i], input[i].length);
+      }
+      return target;
+}
 }
--- 2048Java/test/TwentyFourtyEightTests.java
+++ 2048Java/test/TwentyFourtyEightTests.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
  * Contains and implements test cases for the 2048 game.
  * 
  * @author Péter Ivanics
- * @date 01.01.2016.
+ * @date 01.02.2016.
  */
 public class TwentyFourtyEightTests 
 {
@@ -29,12 +29,340 @@ public class TwentyFourtyEightTests
      * Tests if a newly created board contains exactly two tiles of twos.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     @Test
     public void testEmptyBoardContainsTwoTwos()
     {
+        assertTrue(this.contains(this.game.getBoard(), 2));
         assertEquals(this.count(this.game.getBoard(), 2), 2);
+    }
+    
+    /**
+     * Tests if a newly initialized game is not over.
+     * 
+     * @author Péter Ivanics
+     * @date 01.02.2016.
+     */
+    @Test
+    public void testIfEmptyGameIsNotOver()
+    {
+        assertFalse(this.game.isOver());
+    }
+    
+    /**
+     * Tests that the game is over with a full list when there are no possible moves left.
+     * 
+     * @author Peter Ivanics
+     * @date 02.02.2016.
+     */
+    @Test
+    public void testIfFullBoardGameIsOver()
+    {
+        int[][] testBoard = {
+            {2, 4, 8, 2},
+            {8, 2, 4, 8},
+            {2, 4, 16, 2},
+            {4, 8, 2, 4}
+        };
+        
+        this.game = new TwentyFourtyEight(testBoard);
+        assertTrue(this.game.isOver());
+    }
+    
+    /**
+     * Tests that the game is not over with a full list when there is at least one possible move left.
+     * 
+     * @author Peter Ivanics
+     * @date 02.02.2016.
+     */
+    @Test
+    public void testIfFullBoardWithMovesGameIsNotOver()
+    {
+        int[][] testBoard = {
+            {2, 8, 8, 2}, // the eights should be possible to combine in the first row
+            {8, 2, 4, 8},
+            {2, 4, 16, 2},
+            {4, 8, 2, 4}
+        };
+        
+        this.game = new TwentyFourtyEight(testBoard);
+        assertFalse(this.game.isOver());
+    }
+    
+    /**
+     * Tests combining two neighboring values on a full board.
+     * 
+     * @author Peter Ivanics
+     * @date 21.02.2016.
+     */
+    @Test
+    public void testCombiningTwoValuesOnFullBoard()
+    {
+        int[][] testBoard = {
+            {2, 8, 8, 2}, // the eights should be possible to combine in the first row
+            {8, 2, 4, 8},
+            {2, 4, 16, 2},
+            {4, 8, 2, 4}
+        };
+        
+        this.game = new TwentyFourtyEight(testBoard);
+        this.game.swipeLeft();
+        
+        int[][] expectedBoard = {
+            {2, 16, 2, 0},
+            {8, 2, 4, 8},
+            {2, 4, 16, 2},
+            {4, 8, 2, 4}
+        };
+        
+        System.out.println("Expected:");
+        this.print(expectedBoard);
+        
+        System.out.println("Actual");
+        this.print(this.game.getBoard());
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+     /************** Simple swipe without collision ***********/   
+    /**
+     * Tests swiping a simple board from top to bottom. The board contains two twos only and no collision is made with the swipe.
+     */
+    @Test
+    public void testSimpleSwipeUpNoCollision()
+    {
+        int[][] originalBoard = {
+            {0, 0, 0, 2},
+            {0, 2, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+//        System.out.println("Original:");
+//        this.print(originalBoard);
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeUp();
+        
+        int[][] expectedBoard = {
+            {0, 2, 0, 2},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+//        System.out.println("Expected:");
+//        this.print(expectedBoard);
+        
+//        System.out.println("Actual");
+//        this.print(this.game.getBoard());
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+        /*************************/
+        originalBoard = new int[][] {
+            {2, 0, 0, 0},
+            {0, 2, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+//        System.out.println("Original:");
+//        this.print(originalBoard);
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeUp();
+        
+        expectedBoard = new int[][] {
+            {2, 2, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+//        System.out.println("Expected:");
+//        this.print(expectedBoard);
+        
+//        System.out.println("Actual");
+//        this.print(this.game.getBoard());
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+     /**
+     * Tests swiping a simple board from bottom to top. The board contains two twos only and no collision is made with the swipe.
+     */
+    @Test
+    public void testSimpleSwipeDownNoCollision()
+    {
+        int[][] originalBoard = {
+            {0, 0, 0, 2},
+            {0, 2, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeDown();
+        
+        int[][] expectedBoard = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 2, 0, 2}
+        };
+        
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+    /**
+     * Tests swiping a simple board from left to right. The board contains two twos only and no collision is made with the swipe.
+     */
+    @Test
+    public void testSimpleSwipeRightNoCollision()
+    {
+        int[][] originalBoard = {
+            {0, 0, 0, 2},
+            {0, 2, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeRight();
+        
+        int[][] expectedBoard = {
+            {0, 0, 0, 2},
+            {0, 0, 0, 2},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+    /**
+     * Tests swiping a simple board from right to left. The board contains two twos only and no collision is made with the swipe.
+     */
+    @Test
+    public void testSimpleSwipeLeftNoCollision()
+    {
+        int[][] originalBoard = {
+            {0, 0, 0, 2},
+            {0, 2, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeLeft();
+        
+        int[][] expectedBoard = {
+            {2, 0, 0, 0},
+            {2, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+    /************** Simple swipe with collision ***********/    
+     /**
+     * Tests swiping a simple board from bottom to top. The board contains two twos only which collide during the swipe.
+     */
+    @Test
+    public void testSimpleSwipeUpWithCollision()
+    {
+        int[][] originalBoard = {
+            {2, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {2, 0, 0, 0}
+        };
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeUp();
+        
+        int[][] expectedBoard = {
+            {4, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+     /**
+     * Tests swiping a simple board from top to bottom. The board contains two twos only which collide during the swipe.
+     */
+    @Test
+    public void testSimpleSwipeDownWithCollision()
+    {
+        int[][] originalBoard = {
+            {0, 0, 0, 2},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 2}
+        };
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeDown();
+        
+        int[][] expectedBoard = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 4}
+        };
+        
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+    /**
+     * Tests swiping a simple board from right to left. The board contains two twos only which collide during the swipe.
+     */
+    @Test
+    public void testSimpleSwipeLeftWithCollision()
+    {
+        int[][] originalBoard = {
+            {0, 2, 0, 2},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeLeft();
+        
+        int[][] expectedBoard = {
+            {4, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        assertArrayEquals(expectedBoard, this.game.getBoard());
+    }
+    
+        /**
+     * Tests swiping a simple board from left to right. The board contains two twos only which collide during the swipe.
+     */
+    @Test
+    public void testSimpleSwipeRightWithCollision()
+    {
+        int[][] originalBoard = {
+            {0, 2, 0, 2},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        this.game = new TwentyFourtyEight(originalBoard);
+        this.game.swipeRight();
+        
+        int[][] expectedBoard = {
+            {0, 0, 0, 4},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        
+        assertArrayEquals(expectedBoard, this.game.getBoard());
     }
     
     /******* Private functions *********/
@@ -47,7 +375,7 @@ public class TwentyFourtyEightTests
      * @return True, if the matrix contains the number; false otherwise.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     private boolean contains(int[][] matrix, int number)
     {
@@ -72,7 +400,7 @@ public class TwentyFourtyEightTests
      * @return The number of occurances of the number as an integer.
      * 
      * @author Péter Ivanics
-     * @date 01.01.2016.
+     * @date 01.02.2016.
      */
     private int count(int[][] matrix, int number)
     {
@@ -82,10 +410,29 @@ public class TwentyFourtyEightTests
             for (int j = 0; j < matrix[i].length; j++) 
             {
                 if (matrix[i][j] == number)
-                        return count++;
+                        count++;
             }
         }
         
         return count;
+    }
+    
+    /**
+     * Prints the matrix on the console in a readable format.
+     * @param matrix The matrix to be printed.
+     * 
+     * @author Péter Ivanics
+     * @date 02.02.2016.
+     */
+    private void print(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.length; i++) 
+        {
+            for (int j = 0; j < matrix[i].length; j++) 
+            {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
